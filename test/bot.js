4dd75b58de3a249e59275f257c76d1700837b0c1
Nick Pfisterer <nick@polymoon.net>
2021-09-05 13:25:56 -0700

    Simplify tests

--- test/bot.js
+++ test/bot.js
@@ -1,10 +1,6 @@
 import HieiClient from '../hiei.js'
-import Signale from 'signale'
 
 const client = new HieiClient({
-  commands: './commands',
-  listeners: './listeners',
-  logger: new Signale(),
   options: {
     allowedMentions: [],
     intents: []
