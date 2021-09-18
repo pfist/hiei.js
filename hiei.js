041432801604e7de8b6cef2c5a4c3513fba63b8f
Nick Pfisterer <nick@polymoon.net>
2021-09-17 19:25:45 -0700

    Handle message commands

--- hiei.js
+++ hiei.js
@@ -1,3 +1,4 @@
 export { default as HieiClient } from './lib/HieiClient.js'
 export { default as CommandHandler } from './lib/commands/CommandHandler.js'
+export { default as MessageCommand } from './lib/commands/MessageCommand.js'
 export { default as SlashCommand } from './lib/commands/SlashCommand.js'
---
+++ test/commands/message/reverse.js
@@ -0,0 +1,15 @@
+import { MessageCommand } from '../../../hiei.js'
+
+class Reverse extends MessageCommand {
+  constructor () {
+    super({
+      name: 'reverse'
+    })
+  }
+
+  async run (interaction, message) {
+    await interaction.reply({ content: message.content.split('').reverse().join(''), ephemeral: true })
+  }
+}
+
+export default Reverse
