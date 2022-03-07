08e41c0c7df0a02b60bfca4ccf3c76c4adbc5794
Nick Pfisterer <nick@polymoon.net>
2022-03-07 14:32:25 -0800

    :recycle: Remove custom directories and debug flag

--- test/bot.js
+++ test/bot.js
@@ -2,10 +2,8 @@ import { HieiClient } from '../hiei.js'
 import { Intents } from 'discord.js'
 
 const client = new HieiClient({
-  options: {
-    allowedMentions: [],
-    intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES]
-  }
+  allowedMentions: [],
+  intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES]
 })
 
 client.login(process.env.TOKEN)
