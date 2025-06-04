122a0e1d175b6d9bf4d1fae511af839bf7642151
Nick Pfisterer <guardian.flame9641@fastmail.com>
2025-06-04 16:49:31 -0700

    Event hook listener test

---
+++ test/src/events/evt.interaction-completed.js
@@ -0,0 +1,11 @@
+export default {
+  name: 'interaction:completed',
+  emitter: 'hiei',
+  execute (interaction) {
+    const start = interaction.createdAt
+    const end = Date.now()
+    const elapsedMilliseconds = end - start
+    const elapsedSeconds = (elapsedMilliseconds / 1000).toFixed(2)
+    console.log(`[hiei:events] Interaction ${interaction.commandName} completed successfully in ${elapsedSeconds}s.`)
+  }
+}
