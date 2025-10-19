c8e2f96d32c81172d69acc012113ecad99d98fbd
Nick Pfisterer <guardian.flame9641@fastmail.com>
2025-10-18 19:04:14 -0700

    Remove unused exports

--- src/hiei.js
+++ src/hiei.js
@@ -1,12 +1,2 @@
-// export { HieiClient } from './hiei-client.js'
-// export { InteractionHandler } from './interactions/interaction-handler.js'
-// export { SlashCommand } from './interactions/slash-command.js'
-// export { MessageCommand } from './interactions/message-command.js'
-// export { UserCommand } from './interactions/user-command.js'
-// export { ModalSubmission } from './interactions/modal-submission.js'
-// export { ButtonResponse } from './interactions/button-response.js'
-// export { Listener } from './listeners/listener.js'
-// export { ListenerHandler } from './listeners/listener-handler.js'
-
 export { createInteractionHandler } from './handlers/interaction-handler.js'
 export { createEventHandler } from './handlers/event-handler.js'
