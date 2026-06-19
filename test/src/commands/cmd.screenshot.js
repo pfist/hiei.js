16d0a4cceaa10d7ad8ac6bb128fcdeb81eded204
Nick Pfisterer <guardian.flame9641@fastmail.com>
2026-06-19 10:23:00 -0700

    Add test command for attachment, bool

--- test/src/commands/cmd.screenshot.js
+++ test/src/commands/cmd.screenshot.js
@@ -2,21 +2,29 @@ import { MessageFlags, PermissionFlagsBits } from 'discord.js'
 
 export default {
   interaction: 'slash',
-  name: 'debug',
-  description: 'A command for testing many command options',
+  name: 'screen',
+  description: 'Test command for attachment, boolean',
   options: [
     {
       type: 'attachment',
       name: 'screenshot',
       description: 'An example attachment option',
       required: true
+    },
+    {
+      type: 'boolean',
+  		name: 'spoiler',
+  		description: 'An example boolean option',
+  		required: true
     }
   ],
   permissions: PermissionFlagsBits.SendMessages,
   async execute({ interaction }) {
     const screenshot = interaction.options.getAttachment('screenshot')
+    const spoiler = interaction.options.getBoolean('spoiler')
+
     await interaction.reply({
-      content: `Here is ${screenshot.name} at \`${screenshot.url}\``,
+      content: `${screenshot.name} uploaded successfully. ${spoiler ? 'DO NOT SHARE' : 'Nice.'}`,
       files: [screenshot],
       flags: MessageFlags.Ephemeral
     })
