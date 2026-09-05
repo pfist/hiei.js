593f7da15e556fa966c0cb74caaf55eaa769fb79
Nick Pfisterer <guardian.flame9641@fastmail.com>
2026-09-05 11:46:44 -0700

    Remove scope from logger

--- src/utilities/log-util.js
+++ src/utilities/log-util.js
@@ -1,13 +1,13 @@
 const production = process.env.NODE_ENV === 'production'
 
-export function info (scope, message, ...args) {
-  if (!production) console.log(`[hiei:${scope}] ${message}`, ...args)
+export function info (message, ...args) {
+  if (!production) console.log(`[hiei] ${message}`, ...args)
 }
 
-export function warn (scope, message, ...args) {
-  if (!production) console.warn(`[hiei:${scope}] ${message}`, ...args)
+export function warn (message, ...args) {
+  if (!production) console.warn(`[hiei] ${message}`, ...args)
 }
 
-export function error (scope, message, ...args) {
-  if (!production) console.error(`[hiei:${scope}] ${message}`, ...args)
+export function error (message, ...args) {
+  if (!production) console.error(`[hiei] ${message}`, ...args)
 }
