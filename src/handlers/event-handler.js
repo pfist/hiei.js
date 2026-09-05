70e15c8eeb7c0e4a5d0e4c5735cd85df75c31ef5
Nick Pfisterer <guardian.flame9641@fastmail.com>
2026-09-05 12:10:30 -0700

    Refactor event handler

--- src/handlers/event-handler.js
+++ src/handlers/event-handler.js
@@ -1,48 +1,49 @@
-import { existsSync } from 'node:fs'
+import { statSync } from 'node:fs'
 import { resolve } from 'node:path'
 import { pathToFileURL } from 'node:url'
 import { discoverFiles } from '../utilities/file-util.js'
 import * as log from '../utilities/log-util.js'
 import { dispatch } from './dispatch.js'
 
-export async function createEventHandler (client, { eventDirectory = './src/events' }) {
-  const eventsPath = resolve(process.cwd(), eventDirectory)
-  const eventsPathRelative = eventsPath.startsWith(process.cwd()) ? `.${eventsPath.slice(process.cwd().length)}` : eventsPath
+export async function createEventHandler (client, config) {
+  const eventsPath = config.eventsDirectory ? resolve(config.eventsDirectory) : null
 
-  if (!existsSync(eventsPath)) {
-    log.warn('setup', 'There is no events directory. Event listeners will be disabled.')
-    return
-  }
-
-  const files = await discoverFiles(eventsPath)
-  if (!files.length) {
-    return log.warn('setup', `Events directory is empty: ${eventsPathRelative}`)
-  }
-
-  for (const file of files) {
-    try {
-      const { default: listener } = await import(pathToFileURL(file))
-
-      if (!listener?.event || !listener.emitter || typeof listener.execute !== 'function') {
-        log.warn('setup', `Invalid event listener is missing event name, emitter, and/or execute method: ${file}`)
-        continue
-      }
-
-      const emitter = listener.emitter === 'client' ? client : listener.emitter === 'hiei' ? dispatch : null
-      if (!emitter) {
-        log.warn('setup', `Unknown emitter ${listener.emitter} in event listener: ${file}`)
-        continue
-      }
-
-      if (listener.once) {
-        emitter.once(listener.event, (...args) => listener.execute(...args, client))
-      } else {
-        emitter.on(listener.event, (...args) => listener.execute(...args, client))
-      }
-    } catch (error) {
-      log.error('setup', `Failed to load event listener: ${file}\n`, error)
+  if (eventsPath) {
+    if (!statSync(eventsPath).isDirectory()) {
+      log.warn(`Events directory ${eventsPath.slice(process.cwd().length)} not found.`)
+      return
     }
-  }
 
-  log.info('setup', `Loaded ${files.length} ${files.length === 1 ? 'event' : 'events'} from ${eventsPathRelative}`)
+    const files = await discoverFiles(eventsPath)
+    if (!files.length) {
+      return log.warn(`Events directory ${eventsPath.slice(process.cwd().length)} is empty.`)
+    }
+
+    for (const file of files) {
+      try {
+        const { default: listener } = await import(pathToFileURL(file))
+
+        if (!listener?.event || !listener.emitter || typeof listener.execute !== 'function') {
+          log.warn(`Invalid event listener is missing event name, emitter, and/or execute method: ${file}`)
+          continue
+        }
+
+        const emitter = listener.emitter === 'client' ? client : listener.emitter === 'hiei' ? dispatch : null
+        if (!emitter) {
+          log.warn(`Unknown emitter ${listener.emitter} in event listener: ${file}`)
+          continue
+        }
+
+        if (listener.once) {
+          emitter.once(listener.event, (...args) => listener.execute(...args, client))
+        } else {
+          emitter.on(listener.event, (...args) => listener.execute(...args, client))
+        }
+      } catch (error) {
+        log.error(`Failed to load event listener: ${file}\n`, error)
+      }
+    }
+
+    log.info(`Loaded ${files.length} ${files.length === 1 ? 'event' : 'events'} from ${eventsPath.slice(process.cwd().length)}`)
+  }
 }
