632def2d069ce4e340f3f1dee2e25ddf7a98d46b
Nick Pfisterer <guardian.flame9641@fastmail.com>
2026-09-09 15:23:46 -0700

    Pass interactions directly instead of constructing a new object

--- test/src/events/evt.interaction-failed.js
+++ test/src/events/evt.interaction-failed.js
@@ -1,7 +1,7 @@
 export default {
   event: 'interaction:failed',
   emitter: 'hiei',
-  execute ({ interaction }) {
+  execute (interaction) {
     console.log(`[hiei:events] Interaction ${interaction.commandName} failed.`)
   }
 }
